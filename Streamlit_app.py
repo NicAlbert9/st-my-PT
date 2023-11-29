816730e8be85e552641e3af1f24cd8a73e0591f3
NicAlbert9 <152366813+NicAlbert9@users.noreply.github.com>
2023-11-29 14:33:56 +0800

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -1,1 +1,7 @@
-print('Hello World')
+import streamlit as st
+st.set_page_config(page_title='My Webpage', page_icon=':tada:', layout='wide')
+
+st.subheader('Hi i am Nic Albert :wave:')
+st.title('Freshmen of SNSU')
+st.write('A Computer Eningeering Student')
+st.write('Section 1B')
